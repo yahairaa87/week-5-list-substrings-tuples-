835bf6b3a64cc77fc1def6dff0513d2b8a2ee4d7
yahaira87 <109757636+yahairaa87@users.noreply.github.com>
2022-09-19 17:58:24 +0000

    character placements

--- main.py
+++ main.py
@@ -109,8 +109,8 @@ print(text2[8::3])
 # Reverses the position of all the characters in the following sentence and displays the result on the screen.
 
 text3 = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
-# print(text3.reverse())
-
+#print(text3.reverse())
+print(text3[::-1])
 # Extract the first word of the following sentence using slicing, and display it on the screen:
 sentence ="Controlling complexity is the essence of programming"
 
